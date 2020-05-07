d47c82c7e7c39c755fb5b516fa7903b4276ecb74
学习中的小黑兔 <623291482@qq.com>
2020-05-07 21:07:52 +0800

    添加了锁定相机范围的函数

--- kill the rabbit/Assets/scripts/CameraFollow.cs
+++ kill the rabbit/Assets/scripts/CameraFollow.cs
@@ -6,6 +6,9 @@ public class CameraFollow : MonoBehaviour
 {
     public Transform target;//坐标
     public float smoothing;//使相机行走平滑的因子
+    public Vector2 minPosition;//相机范围最小的坐标
+    public Vector2 maxPosition;//相机范围最大的坐标
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,14 +22,16 @@ public class CameraFollow : MonoBehaviour
         {
             if(transform.position != target.position)//如果相机坐标和玩家坐标不同
             {
-                Vector3 targetPos = target.position;
+                Vector3 targetPos = target.position;//获取玩家现在的坐标
+                targetPos.x = Mathf.Clamp(targetPos.x, minPosition.x, maxPosition.x);
+                targetPos.y = Mathf.Clamp(targetPos.y, minPosition.y, maxPosition.y);
                 transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);//线性差值函数
             }
         }
     }
-    // Update is called once per frame
-    void Update()
+    public void SetCamPosLimit(Vector2 minPos, Vector2 maxPos)//设置相机限制
     {
-        
+        minPosition = minPos;
+        maxPosition = maxPos;
     }
 }
