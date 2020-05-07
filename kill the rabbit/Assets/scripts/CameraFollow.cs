f82114749562225b97c6859496ec76a95a142360
学习中的小黑兔 <623291482@qq.com>
2020-05-07 19:33:03 +0800

    添加了相机跟随功能及其脚本，丰富了地形和怪物移动范围

---
+++ kill the rabbit/Assets/scripts/CameraFollow.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;//坐标
+    public float smoothing;//使相机行走平滑的因子
+    // Start is called before the first frame update
+    void Start()
+    {
+        smoothing = 0.1f;
+    }
+
+    void LateUpdate()
+    {
+        if(target != null)//如果玩家还存在着
+        {
+            if(transform.position != target.position)//如果相机坐标和玩家坐标不同
+            {
+                Vector3 targetPos = target.position;
+                transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);//线性差值函数
+            }
+        }
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
